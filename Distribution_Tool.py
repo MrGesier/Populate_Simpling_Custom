6faa2fdeec297f2c191679ecb333a855667d1d5e
MrGesier <33580568+MrGesier@users.noreply.github.com>
2025-01-07 12:23:45 +0100

    Update Distribution_Tool.py

--- Distribution_Tool.py
+++ Distribution_Tool.py
@@ -1,7 +1,9 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib as plt
+import matplotlib 
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
 from scipy.stats import truncnorm
 
 # Streamlit Configuration
